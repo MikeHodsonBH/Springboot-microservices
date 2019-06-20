6ea6bb774a0665fb5ead0e862e138efc478a9067
Daniel Mburu <junem3ta@gmail.com>
2019-06-20 17:12:30 +0300

    Made with STS IDE

--- main/java/apps/junem3ta/springbootstarter/profilemodel/Profile.java
+++ main/java/apps/junem3ta/springbootstarter/profilemodel/Profile.java
@@ -1,9 +1,5 @@
 package apps.junem3ta.springbootstarter.profilemodel;
 
-/*import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;*/
-
 public class Profile {
 	
 	private String id;
@@ -51,25 +47,6 @@ public class Profile {
     public void setEmail(String email) {
         this.email = email;
     }
-    
-    /*
-    
-    Profile p1 = new Profile("1209","Sheila","Vinson","sheilavinson@profilemgr.tst");
-	Profile p2 = new Profile("1345","Horton","Stafford","hortonstafford@profilemgr.tst");
-	Profile p3 = new Profile("5678","Mcpherson","Rocha","mcphersonrocha@profilemgr.tst");
-	Profile p4 = new Profile("3745","Dixon","Bradley","dixonbradley@profilemgr.tst");
-	Profile p5= new Profile("4532","Mays","Cortez","mayscortez@profilemgr.tst");
-	Profile p6 = new Profile("3421","Angelita","Parsons","angelitaparsons@profilemgr.tst");
-	Profile p7 = new Profile("3452", "Neva", "Erickson", "nevaerickson@profilemgr.tst");  
-	Profile p8 = new Profile("1345", "Clara", "Hicks", "clarahicks@profilemgr.tst");
-	Profile p9 = new Profile("3541", "Huffman", "Goodman", "huffmangoodman@profilemgr.tst");
-	Profile p10 = new Profile("2455", "Adriana", "Meyers", "adrianameyers@profilemgr.tst");
-	Profile p11 = new Profile("2355", "Sonia", "Horn", "soniahorn@profilemgr.tst");
-	Profile p12 = new Profile("2163", "Adkins", "Bell", "adkinsbell@profilemgr.tst");
-	Profile p13 = new Profile("2576", "Pitts", "Forbes", "pittsforbes@profilemgr.tst");
-	Profile p14 = new Profile("1222", "Brittney", "Holden", "brittneyholden@profilemgr.tst");
-
-	 */
 }
 
 
