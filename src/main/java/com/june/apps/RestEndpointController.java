f2ffd4d3c589b43637c67bf713fa84de18c18934
Daniel Mburu <junem3ta@gmail.com>
2019-07-25 07:57:37 +0300

    Added HttpRequestHeader Interceptor | Validate Source App

--- src/main/java/com/june/apps/RestEndpointController.java
+++ src/main/java/com/june/apps/RestEndpointController.java
@@ -4,20 +4,23 @@ import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.june.apps.schemas.customer.*;
+import com.june.apps.schemas.customer.GetUserRequest;
+import com.june.apps.schemas.customer.GetUserResponse;
 
 @RestController
+@RequestMapping("/customerWS")
 public class RestEndpointController {
 	
 	private final Logger logger = LoggerFactory.getLogger(this.getClass());
@@ -26,16 +29,13 @@ public class RestEndpointController {
 	public SOAPConnector soapConnector;
 	
 	@PostMapping("/getCustomer")
-	public GetUserResponse user(@RequestParam String userName) {
+	public GetUserResponse user(@RequestBody GetUserRequest userObj) {
 		logger.debug("Debug message");
 		logger.info("Info message");
 		logger.warn("Warn message");
 		logger.error("Error message");
 		
-		
-		GetUserRequest request = new GetUserRequest();
-		request.setName(userName);
-		GetUserResponse response = (GetUserResponse)soapConnector.callWebService("http://localhost:9090/customerWS/users", request);
+		GetUserResponse response = (GetUserResponse)soapConnector.callWebService("http://localhost:9090/customerWS/users", userObj);
 		return response;
 	}
 	
@@ -51,7 +51,7 @@ public class RestEndpointController {
 		responseObj.setHeaders(requestHeaders);
 	}
 	
-	@RequestMapping("/")
+	@GetMapping("/getGraphicProcessors")
 	public ResponseEntity<GraphicsProcessors> defaultEndpoint(@RequestHeader Map<String, String> headerObj) {
 		
 		/*Custom Headers*/
@@ -63,20 +63,28 @@ public class RestEndpointController {
 		return new ResponseEntity<GraphicsProcessors>(responseObj, httpHeaders, HttpStatus.OK);
 	}
 	
-	@RequestMapping("/404d")
+	@GetMapping("/404d")
 	public ResponseEntity<Void> httpTestStatus() {		
 		return new ResponseEntity<Void>(HttpStatus.BAD_REQUEST);
 	}
 	
-	@RequestMapping("/validateSourceApp")
+	@GetMapping("/validateSourceApp")
+	public String interceptorTest(@RequestHeader Map<String, String> headersObj, @RequestParam String param) {
+		return String.format("Auth key is: %s || Invoking System is %s ", headersObj.get("user-auth-key"), headersObj.get("app-name"));
+	}
+	
 	/*
+	 * public String interceptorTest(@RequestHeader("user-auth-key") String authkey, @RequestBody String param) {
+		return String.format("Auth key is %s :", authkey) + "Request Param = " + param;
+	}
 	 * defaultValue can be used optionally to set default value for optional header
 	 * check (optionalHeader.equals(3600)) to determine whether the header has been provided.
 	 * @RequestHeader(value = "optional-header", defaultValue = "3600") int optionalHeader)
-	*/
-	public ResponseEntity<String> evaluateRequiredHeader(@RequestHeader(value="App-Name", required = false) String appName) {
+	 * public ResponseEntity<String> evaluateRequiredHeader(@RequestHeader(value="App-Name", required = false) String appName) {
 		return new ResponseEntity<String>((appName.equals("Cool App") || appName.equals("Grand Aries") || appName.equals("Ancient Stitch")) ? "Valid" : "Nice Try :D ", HttpStatus.OK);
 	}
+	*/
+	
 }
 
 
