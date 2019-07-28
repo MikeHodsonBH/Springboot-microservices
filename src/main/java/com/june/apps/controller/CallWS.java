8fbbfea7a0dd5f3583fbeb53c1377788c7574c55
Daniel Mburu <junem3ta@gmail.com>
2019-07-28 20:01:08 +0300

    Add soap header to request envelope

--- src/main/java/com/june/apps/controller/CallWS.java
+++ src/main/java/com/june/apps/controller/CallWS.java
@@ -1,7 +1,10 @@
 package com.june.apps.controller;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
+import javax.xml.namespace.QName;
 import javax.xml.soap.MessageFactory;
 import javax.xml.soap.MimeHeaders;
 import javax.xml.soap.SOAPBody;
@@ -71,27 +74,29 @@ public class CallWS {
 		/* Soap Message */
 		MessageFactory mfc = MessageFactory.newInstance();
 		SOAPMessage soapMessage = mfc.createMessage();
-		String ns = "us";
-		String nsURI = "http://june.com/customerwsdlmodel";
 
 		SOAPPart soapPart = soapMessage.getSOAPPart();
 		SOAPEnvelope envelope = soapPart.getEnvelope();
-		envelope.addNamespaceDeclaration(ns, nsURI);
+		envelope.addNamespaceDeclaration("us", "http://june.com/customerwsdlmodel");
+		envelope.addNamespaceDeclaration("xsd", "http://www.w3.org/2001/XMLSchema");
+		envelope.addNamespaceDeclaration("xsi", "http://www.w3.org/2001/XMLSchema-instance");
+		envelope.addNamespaceDeclaration("m", "http://antispam.webservice.optenet.com");
 
 		SOAPBody soapBody = envelope.getBody();
-		SOAPElement soapBodyElement = soapBody.addChildElement("getUserRequest", "us");
-		SOAPElement soapElementChild = soapBodyElement.addChildElement("name", "us");
+		SOAPElement soapBodyElement = soapBody.addChildElement("ValidateUser", "m").addAttribute(envelope.createName("xmlns:n"), "http://antispam.webservice.optenet.com");
+		
+		SOAPElement soapElementChild = soapBodyElement.addChildElement("String", "xsi", "uri");
 		soapElementChild.addTextNode("NinaPark");
 
 		MimeHeaders headers = soapMessage.getMimeHeaders();
 		headers.addHeader("SOAPAction", "http://june.com/customerwsdlmodel/users");
 		
-		/*
-		 * System.out.println("Request SOAP Message:"); 
-		 * soapMessage.writeTo(System.out);
-		 * System.out.println("\n");
-		 * System.out.println(soapMessage.getMimeHeaders().getHeader("SOAPAction"));
-		 */
+		
+		  System.out.println("Request SOAP Message:"); 
+		  soapMessage.writeTo(System.out);
+		  System.out.println("\n");
+		  System.out.println(soapMessage.getMimeHeaders().getHeader("SOAPAction"));
+		 
 		
 		/* End Soap Envelope */
 		
@@ -100,27 +105,83 @@ public class CallWS {
 	}
 
 	@GetMapping("saajCall")
-	public ResponseEntity<SOAPEnvelope> callWs() throws Exception {
+	public SOAPEnvelope /*ResponseEntity<SOAPEnvelope>*/ callWs() throws Exception {
 
 		String endpoint = "http://localhost:9090/customerWS/users";
-
-		SOAPMessage msg = createSoapRequest();
-
-		/* Call WS */
-		try {
-			HttpHeaders responseHeaders = new HttpHeaders();
-			responseHeaders.setContentType(MediaType.TEXT_XML);
 		
-			
-			SOAPConnectionFactory connectionFactory = SOAPConnectionFactory.newInstance();
-			SOAPConnection soapConnection = connectionFactory.createConnection();
+		final String rBody = "<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:us=\"http://june.com/customerwsdlmodel\">" +
+				"<soapenv:Header/>" + 
+				"<soapenv:Body>" + 
+				"<us:getUserRequest>" + 
+				"<us:name>BullockGriffith</us:name>" + 
+				"</us:getUserRequest>" + 
+				"</soapenv:Body>" + 
+				"</soapenv:Envelope>";
+		
+		InputStream is = new ByteArrayInputStream(rBody.getBytes());
+		SOAPMessage request = MessageFactory.newInstance().createMessage(null, is);
+		System.out.println("Request SOAP Message:"); 
+		request.writeTo(System.out);
+		System.out.println("\n");
+		
+		try { 
+			HttpHeaders responseHeaders = new HttpHeaders();
+		  responseHeaders.setContentType(MediaType.TEXT_XML);
+		  
+		  
+		  SOAPConnectionFactory connectionFactory =
+		  SOAPConnectionFactory.newInstance(); SOAPConnection soapConnection =
+		  connectionFactory.createConnection();
+		  
+		  SOAPMessage response = soapConnection.call(request, endpoint); 
+		  return response.getSOAPPart().getEnvelope();
+		  
+		  } catch (Exception e) { return null; }
+		 
+		/* End Soap Call */
 
-			SOAPMessage response = soapConnection.call(msg, endpoint);
-			return ResponseEntity.ok().body(response.getSOAPPart().getEnvelope());
+	}
+	
+	@GetMapping("saajCallWH")
+	public WSResponse /*ResponseEntity<SOAPEnvelope>*/ callWSWH() throws Exception {
 
-		} catch (Exception e) {
-			return null;
-		}
+		String endpoint = "http://localhost:9090/customerWS/users";
+		
+		final String rBody = "<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:us=\"http://june.com/customerwsdlmodel\">" +
+				"<soapenv:Header/>" + 
+				"<soapenv:Body>" + 
+				"<us:getUserRequest>" + 
+				"<us:name>BullockGriffith</us:name>" + 
+				"</us:getUserRequest>" + 
+				"</soapenv:Body>" + 
+				"</soapenv:Envelope>";
+		
+		InputStream is = new ByteArrayInputStream(rBody.getBytes());
+		SOAPMessage request = MessageFactory.newInstance().createMessage(null, is);
+		System.out.println("Request SOAP Message:"); 
+		request.writeTo(System.out);
+		System.out.println("\n");
+		
+		try {
+			  SOAPConnectionFactory connectionFactory = SOAPConnectionFactory.newInstance(); 
+			  SOAPConnection soapConnection = connectionFactory.createConnection();
+			  
+			  if (request.getSOAPPart().getEnvelope().getHeader() != null) {
+				  request.getSOAPPart().getEnvelope().getHeader().detachNode();
+			  }
+			  
+			  request.getSOAPPart().getEnvelope().addHeader().addHeaderElement(new QName("header","headervalue"));
+			  request.getSOAPPart().getEnvelope().getHeader().addHeaderElement(new QName("headerII","headerIIvalue"));
+			  request.saveChanges();
+			  
+			  SOAPMessage response = soapConnection.call(request, endpoint); 
+			  soapConnection.close();
+			  return new WSResponse(response.getSOAPPart().getEnvelope(), "Success", "null");
+			  
+		  } catch (Exception e) {
+			  return new WSResponse(null, "Failed", e.toString());
+		  }
+		 
 		/* End Soap Call */
 
 	}
